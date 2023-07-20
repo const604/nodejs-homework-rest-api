d7d37f41c40a53f01f08589cea93fa9c83c1d7b3
const604 <116814907+const604@users.noreply.github.com>
2023-07-20 13:34:17 +0300

    2

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,11 +4,11 @@ exports.getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const { favorite = true} = req.query;
+  // const { favorite = true} = req.query;
   const allContacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
-    favorite,
+    // favorite,
   }).populate("owner", "name email");
   // const allContacts = await Contact.find({}, "name phone");
   // const allContacts = await Contact.find({}, "+password");
